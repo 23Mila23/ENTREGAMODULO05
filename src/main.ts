4c51240b2cedccc1ca24a2d830b79c3755a3eea9
Camila Villalba <cami_vp_23@hotmail.com>
2024-07-26 10:51:14 +0200

    add functions plantarse and nueva partida

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
 let puntuacion = 0;
-
+const puntuacionMaxima = 7.5;
 const muestraPuntuacion = () => {
   const muestraP = document.getElementById("puntuacion") as HTMLElement;
   muestraP.innerHTML = puntuacion.toString();
 };
-
 document.addEventListener("DOMContentLoaded", muestraPuntuacion);
-
 const dameCarta = () => {
   let numero = Math.ceil(Math.random() * 10);
   if (numero > 7) {
     numero = numero + 2;
   }
   muestraCarta(numero);
-
-  if(numero >= 10){
+  if (numero >= 10) {
     puntuacion += 0.5;
-  } else{
+  } else {
     puntuacion += numero;
   }
-  muestraPuntuacion()
-};
+  muestraPuntuacion();
 
-const botonDameCarta = document.getElementById("dame-carta");
+  controlarGameOver();
+};
+const botonDameCarta = document.getElementById(
+  "dame-carta"
+) as HTMLButtonElement;
 botonDameCarta?.addEventListener("click", dameCarta);
 
 const muestraCarta = (carta: number): void => {
@@ -46,26 +46,73 @@ const muestraCarta = (carta: number): void => {
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
       break;
     case 5:
-      imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
       break;
     case 6:
-      imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
       break;
     case 7:
-      imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
       break;
     case 10:
-      imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
       break;
     case 11:
-      imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
       break;
     case 12:
-      imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
       break;
 
     default:
-     imagenCarta.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg"
+      imagenCarta.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
       break;
+  }
+};
+const mePlanto = () => {
+  let mensaje = "";
+  if (puntuacion <= 4) {
+    mensaje = "Has sido muy conservador";
+  }
+  if (puntuacion > 4) {
+    mensaje = "Te ha entrado el canguelo eh";
+  }
+  if (puntuacion >= 6 || puntuacion >= 7) {
+    mensaje = "Casi casi...";
+  }
+  if (puntuacion == 7.5) {
+    mensaje = "¡ Lo has clavado! ¡Enhorabuena!";
+  }
+  const getMePlanto = document.getElementById("mensaje") as HTMLElement;
+  getMePlanto.innerHTML = mensaje;
+  botonDameCarta.disabled = true;
+};
+const botonPlantarse = document.getElementById(
+  "plantarse"
+) as HTMLButtonElement;
+botonPlantarse?.addEventListener("click", mePlanto);
+
+const nuevaPartida = () => {
+  location.reload();
+};
+const botonNuevaPartida = document.getElementById(
+  "nueva-partida"
+) as HTMLButtonElement;
+botonNuevaPartida.addEventListener("click", nuevaPartida);
+
+const controlarGameOver = () => {
+  const gameOver = "GAME OVER";
+  if (puntuacion > puntuacionMaxima) {
+    const getGameOver = document.getElementById("mensaje") as HTMLElement;
+    getGameOver.innerHTML = gameOver;
+    botonDameCarta.disabled = true;
+    botonPlantarse.disabled = true;
   }
 };
